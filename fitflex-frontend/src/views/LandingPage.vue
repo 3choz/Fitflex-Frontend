dca2d381a3526bebc8aa83848b2eb72acdf7fcee
Benis <will.jenifer@yahoo.com>
2024-04-15 13:25:59 -0400

    Working on Landing page

--- fitflex-frontend/src/views/LandingPage.vue
+++ fitflex-frontend/src/views/LandingPage.vue
@@ -1,7 +1,25 @@
 <template>
+  
   <div class="home">
     <div>
       <img alt="Woman Stretching" src="../assets/girl-stretching.png">
+      <p>We bring the tools you need to succeed</p>
+      <!-- Add a 'Join Us Button' button  -->
+      
+      <p>Already have an account?</p>
+      <!-- Add 'Login' Button -->
+
+      <p>We provide the following:</p>
+
+      <p>Free pre-approved workouts with difficulty levels. </p>
+      <!-- Add workout icon -->
+      <!-- Add checkmark icon -->
+      <p>Progress monitoring to reflect your journey</p>
+      <p>Opportunity to provide feedback to improve your experience.</p>
+      <!-- Add message icon -->
+      <!-- Add King Icon -->
+      <p> Goal tracking and statistical summary</p>
+
     </div>
     
     Landing Page
@@ -22,3 +40,13 @@ export default {
   }
 }
 </script>
+
+<style>
+  p{
+    /* color: red; */
+    border: 1px solid black;
+    border-radius: 5px;
+    width: 500px;
+    height: 30px;
+  }
+</style>
