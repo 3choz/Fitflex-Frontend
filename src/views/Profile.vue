ba010b5bfeca0df69b7763e9171d90dd37f871d9
Jared <jemraider21@gmail.com>
2024-04-30 14:25:46 +0000

    Implemented functionality to update a user's information

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,26 +1,3 @@
-<!-- <template>
-  <br>
-    <div class="profile" id="profile">
-      <form>
-        <p>Buffed_Duck</p>
-        <p>Username: <input type="text" required v-model="itemName"></p>
-        <p>First Name: <input type="text" v-model="itemNumber"></p>
-        <p>Last Name: <input type="text" v-model="itemNumber"></p>
-        <p>Email: <input type="text" v-model="itemNumber"></p>
-        <p>Phone Number: <input type="text" v-model="itemNumber"></p>
-        <p>Date of Birth: <input type="text" v-model="itemNumber"></p>
-        <p>Password: <input type="password" v-model="itemNumber"></p>
-        
-        <button type="submit">Save Changes</button>
-      </form>
-      <br>
-      <button type="submit" class="blue-button">Delete Account</button>
-
-    </div>
-  </template> -->
-
-<!-- -- -->
-
 <template>
   <div class="update-profile">
     <div v-if="!editUser" class="register update-profile user-auth-input-box">
@@ -38,68 +15,47 @@
       <router-link to="/profile/updatepassword" class="button-link">Change Password</router-link>
     </div>
     <div v-else class="register update-profile user-auth-input-box">
-      <div v-if="!changePassword">
-        <h2>Edit your information</h2>
-        <form @submit.prevent="submitForm">
+      <h2>Edit your information</h2>
+      <form @submit.prevent="updateUserInformation">
+        <div>
           <div>
-            <div>
-              <label for="firstName">First Name</label>
-              <input v-model="firstName" type="text" placeholder="First Name">
-            </div>
-            <div>
-              <label for="lastName">Last Name</label>
-              <input v-model="lastName" type="text" placeholder="Last Name">
-            </div>
+            <label for="firstName">First Name</label>
+            <input v-model="firstName" type="text" placeholder="First Name">
           </div>
           <div>
-            <label for="dob">Date of Birth</label>
-            <input v-model="dateOfBirth" type="date">
+            <label for="lastName">Last Name</label>
+            <input v-model="lastName" type="text" placeholder="Last Name">
           </div>
-          <div>
-            <label for="sex">Sex</label>
-            <select v-model="sex" placeholder="Sex" name="sex">
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Other">Other</option>
-            </select>
-          </div>
-
-          <div>
-            <label for="phoneNumber">Phone Number</label>
-            <input v-model="phoneNumber" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number">
-          </div>
-          <br>
-          <button class="button-link" type="submit">Save Changes</button>
-          <button class="button-link" @click="editUser = false">Cancel</button>
-        </form>
-      </div>
-      <div v-else>
-        <h2>Change {{ user.getFirstName() }}'s Password</h2>
-        <form @submit="updatePassword">
-          <div>
-            <label for="currentPassword">Current Password</label>
-            <input v-model="currentPassword" type="text" placeholder="Password" required>
-          </div>
-          <div>
-            <label for="newPassword">New Password</label>
-            <input v-model="newPassword" type="text" placeholder="Password" required>
-          </div>
-          <div>
-            <label for="confirmNewPassword">Confirm New Password</label>
-            <input v-model="confirmNewPassword" type="text" placeholder="Confirm Password" required>
-          </div>
-          <br>
-          <button class="button-link" type="submit">Update Password</button>
-          <button class="button-link" @click="editUser = false; changePassword = false">Cancel</button>
-        </form>
-      </div>
+        </div>
+        <div>
+          <label for="dob">Date of Birth</label>
+          <input v-model="dateOfBirth" type="date">
+        </div>
+        <div>
+          <label for="sex">Sex</label>
+          <select v-model="sex" placeholder="Sex" name="sex">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div>
+          <label for="phoneNumber">Phone Number</label>
+          <input v-model="phoneNumber" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number">
+        </div>
+        <br>
+        <button class="button-link" type="submit">Save Changes</button>
+        <button class="button-link" @click="editUser = false">Cancel</button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 import { getUserAssignedProgram } from '@/utils/api/ProgramsApiUtil';
-import { getUserFromSession, isUserLoggedIn } from '@/utils/session/SessionUtils';
+import { updateUser } from '@/utils/api/UserApiUtil';
+import { getUserFromSession, isUserLoggedIn, saveUserToSession } from '@/utils/session/SessionUtils';
+import { mapActions } from 'vuex';
 
 export default {
   name: 'UserProfile',
@@ -112,10 +68,7 @@ export default {
       lastName: "",
       dateOfBirth: "",
       sex: "",
-      phoneNumber: "",
-      currentPassword: "",
-      newPassword: "",
-      confirmNewPassword: ""
+      phoneNumber: ""
     }
   },
   beforeRouteEnter(to, from, next){
@@ -129,8 +82,39 @@ export default {
     this.assignedProgram = await getUserAssignedProgram(this.user);
   },
   methods: {
+    ...mapActions(['userSignIn']),
     async updateUserInformation() {
-      console.log("Updated user information");
+      if(this.firstName !== ""){
+        this.user.setFirstName(this.firstName);
+      }
+
+      if(this.lastName !== ""){
+        this.user.setLastName(this.lastName);
+      }
+
+      if(this.dateOfBirth !== ""){
+        this.user.setDateOfBirth(this.dateOfBirth);
+      }
+
+      if(this.sex !== ""){
+        this.user.setSex(this.sex);
+      }
+
+      if(this.phoneNumber !== ""){
+        this.user.setPhoneNumber(this.phoneNumber);
+      }
+
+      const updateUserResponse = await updateUser(this.user);
+      if(!updateUserResponse.getIsSuccessful()){
+        const errorMessage = `Error updating user: ${updateUserResponse.getErrorMessage()}`;
+        console.log(errorMessage);
+        alert(errorMessage);
+        return;
+      }
+
+      console.log(updateUserResponse.toString());
+      saveUserToSession(this.user, this.userSignIn, "/profile");
+      this.editUser = false;
     },
   }
 };
