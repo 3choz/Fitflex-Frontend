dd90cd9ed733f3d68e6768ccb8d7777020adde54
Avery Reise <reiseavery@gmail.com>
2024-05-05 14:23:12 -0400

    Adjusted Footer

--- src/components/FooterBelow.vue
+++ src/components/FooterBelow.vue
@@ -2,7 +2,7 @@
     <div id="footer">
       <!-- TODO: Make footer Sticky -->
       <p>Copyright @2024</p> 
-      <p>FitFles LLC. No Trademark</p>
+      <p>FitFlex LLC. No Trademark</p>
 
 
     </div>
