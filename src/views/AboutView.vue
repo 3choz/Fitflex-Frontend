2a00597b1a6dfbe2e643afbf7a2781ea4b70a909
Avery Reise <reiseavery@gmail.com>
2024-05-05 14:42:34 -0400

    Adjusted capitalization

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <br><br>
     <h1>FitFlex</h1>
-    <p class="aboutText">we believe that achieving fitness goals should be flexible and personalized. Our app is designed to empower
+    <p class="aboutText">We believe that achieving fitness goals should be flexible and personalized. Our app is designed to empower
       individuals on their journey to better health by offering a versatile platform that adapts to their unique needs
       and preferences. Whether you're a beginner looking to kickstart your fitness routine or a seasoned athlete
       striving for new heights, FitFlex provides a comprehensive suite of tools, including customizable workout plans,
